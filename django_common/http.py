e9a6228acb9fc4daa1b6b5b83d9e4c5596148880
michaelb <michaelpb@gmail.com>
2012-11-02 21:30:26 -0200

    Adding JsonpResponse object for padded JSON

--- django_common/http.py
+++ django_common/http.py
@@ -13,6 +13,18 @@ class JsonResponse(HttpResponse):
     json = json_response(data=data, errors=errors, success=success)
     super(JsonResponse, self).__init__(json, mimetype='application/json')
 
+class JsonpResponse(HttpResponse):
+  """
+  Padded JSON response, used for widget XSS
+  """
+  def __init__(self, request, data={ }, errors=[ ], success=True):
+    """
+    data is a map, errors a list
+    """
+    json = json_response(data=data, errors=errors, success=success)
+    js = "%s(%s)" % (request.GET.get("jsonp", "jsonp_callback"), json)
+    super(JsonpResponse, self).__init__(js, mimetype='application/javascipt')
+
 def json_response(data={ }, errors=[ ], success=True):
   data.update({
     'errors': errors,
