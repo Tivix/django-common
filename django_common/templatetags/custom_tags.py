e7d0344f1bf09d07a6a9afc93838cb443baf18f2
Adam Lord <adam.lord@gmail.com>
2013-05-17 12:20:38 -0700

    Fixed template error; rewrote fragments to be Bootstrap-compatible

--- django_common/templatetags/custom_tags.py
+++ django_common/templatetags/custom_tags.py
@@ -24,13 +24,17 @@ class FormFieldNode(template.Node):
       return ''
 
     widget = form_field.field.widget
-     if isinstance(widget, widgets.RadioSelect) or isinstance(widget, widgets.CheckboxInput):
-        t = get_template('common/fragments/radio_checkbox_field.html')
+    if isinstance(widget, widgets.RadioSelect):
+        t = get_template('common/fragments/radio_field.html')
     else:
-        if isinstance(widget, widgets.CheckboxSelectMultiple):
-            t = get_template('common/fragments/multi_checkbox_field.html')
+        if isinstance(widget, widgets.CheckboxInput):
+            t = get_template('common/fragments/checkbox_field.html')
         else:
-            t = get_template('common/fragments/form_field.html')
+            if isinstance(widget, widgets.CheckboxSelectMultiple):
+                t = get_template('common/fragments/multi_checkbox_field.html')
+            else:
+                t = get_template('common/fragments/form_field.html')
+
     return t.render(Context({
         'form_field': form_field,
         'help_text': self.help_text,
