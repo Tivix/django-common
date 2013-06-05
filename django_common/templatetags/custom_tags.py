e4f1b69744070e93c065d8236258b27e415a44f0
Dariusz Fryta <dariuszfryta@gmail.com>
2013-06-05 12:02:59 +0200

    Make code more clean

--- django_common/templatetags/custom_tags.py
+++ django_common/templatetags/custom_tags.py
@@ -22,16 +22,15 @@ class FormFieldNode(template.Node):
             return ''
 
         widget = form_field.field.widget
+
         if isinstance(widget, widgets.RadioSelect):
             t = get_template('common/fragments/radio_field.html')
+        elif isinstance(widget, widgets.CheckboxInput):
+            t = get_template('common/fragments/checkbox_field.html')
+        elif isinstance(widget, widgets.CheckboxSelectMultiple):
+            t = get_template('common/fragments/multi_checkbox_field.html')
         else:
-            if isinstance(widget, widgets.CheckboxInput):
-                t = get_template('common/fragments/checkbox_field.html')
-            else:
-                if isinstance(widget, widgets.CheckboxSelectMultiple):
-                    t = get_template('common/fragments/multi_checkbox_field.html')
-                else:
-                    t = get_template('common/fragments/form_field.html')
+            t = get_template('common/fragments/form_field.html')
 
         if self.help_text is None:
             self.help_text = form_field.help_text
