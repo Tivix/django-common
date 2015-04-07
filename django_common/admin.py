2b0e4f79e00b3fb026a1b8926bac351405a0dd82
mjaworski <inferniac@gmail.com>
2015-04-07 11:19:40 +0200

    Fixed django 1.8 compatiblity, upgraded min required Django to 1.6

--- django_common/admin.py
+++ django_common/admin.py
@@ -61,7 +61,7 @@ AdminForm.__init__ = __init__
 class NestedModelAdmin(ModelAdmin):
 
     @csrf_protect_m
-    @transaction.commit_on_success
+    @transaction.atomic
     def add_view(self, request, form_url='', extra_context=None):
         "The 'add' admin view for this model."
         model = self.model
@@ -168,7 +168,7 @@ class NestedModelAdmin(ModelAdmin):
         return self.render_change_form(request, context, form_url=form_url, add=True)
 
     @csrf_protect_m
-    @transaction.commit_on_success
+    @transaction.atomic
     def change_view(self, request, object_id, extra_context=None):
         "The 'change' admin view for this model."
         model = self.model
