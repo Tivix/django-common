5cf51d5e51ae1c3fdd09c75db5aaa0630159fdab
Sultan Imanhodjaev <sultan.imanhodjaev@gmail.com>
2015-06-23 01:46:27 +0600

    Fixes for #23 'I/O operation on closed file' error

--- django_common/scaffold.py
+++ django_common/scaffold.py
@@ -511,22 +511,25 @@ class Scaffold(object):
         with open('{0}{1}/models.py'.format(self.SCAFFOLD_APPS_DIR, self.app), 'r') as fp:
             self.models_file = fp
 
-        # Check if model already exists
-        for line in self.models_file.readlines():
-            if 'class {0}'.format(self.model) in line:
-                self._info('exists\t{0}{1}/models.py'.format(self.SCAFFOLD_APPS_DIR, self.app), 1)
-                return
+            # Check if model already exists
+            for line in self.models_file.readlines():
+                if 'class {0}'.format(self.model) in line:
+                    self._info('exists\t{0}{1}/models.py'.format(self.SCAFFOLD_APPS_DIR, self.app), 1)
+                    return
 
-        self._info('create\t{0}{1}/models.py'.format(self.SCAFFOLD_APPS_DIR, self.app), 1)
-        # Prepare fields
-        self.imports = list()
-        fields = list()
-        for field in self.fields:
-            new_field = self.get_field(field)
-            if new_field:
-                fields.append(new_field)
-                self._info('added\t{0}{1}/models.py\t{2} field'.format(
-                    self.SCAFFOLD_APPS_DIR, self.app, field.split(':')[1]), 1)
+            self._info('create\t{0}{1}/models.py'.format(self.SCAFFOLD_APPS_DIR, self.app), 1)
+
+            # Prepare fields
+            self.imports = []
+            fields = []
+
+            for field in self.fields:
+                new_field = self.get_field(field)
+
+                if new_field:
+                    fields.append(new_field)
+                    self._info('added\t{0}{1}/models.py\t{2} field'.format(
+                        self.SCAFFOLD_APPS_DIR, self.app, field.split(':')[1]), 1)
 
         # Open models.py to append
         with open('{0}{1}/models.py'.format(self.SCAFFOLD_APPS_DIR, self.app), 'a') as fp:
