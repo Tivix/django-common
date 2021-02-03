f88748223175b04004cfbed73e3ab1fa4288c52c
daveusa31 <bakteev2003@yandex.ru>
2021-02-03 17:44:07 +0300

    Detect fields manytomany

--- django_common/models.py
+++ django_common/models.py
@@ -1,14 +1,19 @@
 from django.db import models
 
 
+def check_may_to_many_field(field):
+    return "m2m_reverse_target_field_name" in dir(field)
+
+
 @property
 def __data__(self):
     # noinspection PyProtectedMember
     fields = self.__class__._meta.get_fields()
     data = {
         field.name: getattr(self, field.name)
-        for field in fields 
+        for field in fields
         if not isinstance(field, models.fields.reverse_related.ManyToOneRel)
+        and not check_may_to_many_field(field)
     }
     return data
 
